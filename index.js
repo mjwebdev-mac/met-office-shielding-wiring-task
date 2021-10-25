5bed5604b2256eb92fb3efceedba1ae079f9c169
MJ Mac <mjwebdev@icloud.com>
2021-10-25 14:23:35 +0100

    refactored code to use classes

--- index.js
+++ index.js
@@ -1,106 +1,68 @@
 // Met Office: shielding-and-wiring task
 
 
-
-// ========== task one =========== \\ 
-// === Calculate the Shielding === \\
-// =============================== \\ 
-
-
 // First, parse the given string list into a workable array.
 const boxesListToArray = list => list.split(/\s/g).filter(Boolean)
 
-// Create a function to calculate the total surface area and area of smallest side of each box
-const shieldingPerBox = box => {
+// Box class defines the dimensions of each box, and is reusable for both tasks.
+class Box {
 
-  // parse each string into an array and create three separate values to represent the length, width and height
-  // this value is sorted in ascending order, to help calculate which side to use for the smallest side
-  const boxArr = box.split('x').sort((a, b) => a - b);
+  // Defines the dimensions of each box and assigns variables to each dimension
+  constructor(dimensions) {
+    this.dimensions = dimensions
 
-  // assign identifiers for each side
-  const [side1, side2, side3] = [ boxArr[0] * boxArr[1] * 2,
-                                  boxArr[0] * boxArr[2] * 2,
-                                  boxArr[2] * boxArr[1] * 2, ]
+    // boxSplit delimits the string given into an array with split, and then sorts them in ascending order.
+    // sorting in ascending order allows us to find the smallest value, by accessing the first element in the array.
+    this.boxSplit = dimensions.split('x').sort((a, b) => a - b);
 
-  // calculate the surface area
-  const surfaceArea = side1 + side2 + side3
+    // for easier readability, the smallest, middle and largrest dimensions are assigned identifiers.
+    this.smallestDimension = this.boxSplit[0]
+    this.middleDimension = this.boxSplit[1]
+    this.largestDimension = this.boxSplit[2]
+  }
 
-  // and the smallest side, which is used to calculate the amount of extra slack to provision for each box
-  const smallestSide = boxArr[0] * boxArr[1];
+  get shieldingPerBox() {
+            // first three lines calculate the surface area, and the fourth line calculates the shortest side.
+    return  (this.smallestDimension * this.middleDimension * 2) +
+            (this.smallestDimension * this.largestDimension * 2) +
+            (this.largestDimension * this.middleDimension * 2) +
+            (this.smallestDimension * this.middleDimension)
+          }
 
-  // return the total sheeting required for the shielding
-  return surfaceArea + smallestSide
+  get wiringPerBox() {
+            // first line calculates the volume, the second line calculates the shortest circumferance
+    return  (this.smallestDimension * this.middleDimension * this.largestDimension) +
+            (this.smallestDimension * 2 + this.middleDimension * 2)
+  }
 }
 
-// Now iterate through each box in the array. Return a total sum for the amount of shielding required
-const totalShielding = (list) => { return list
-      // first, convert each string value for the box into the required allowance of sheeting, equal to volume + slack
-                                              .map( box => shieldingPerBox(box))
-      // now calculate the total value of all boxes in the array. 
-                                              .reduce((accumulatedValue, currentBox) => accumulatedValue + currentBox)
+// the CalculateTotal class accepts an array of boxes, and returns the required value of shielding, or
+// wiring, depending on the method passed when called.
+class CalculateTotal {
+  constructor(boxes) {
+    this.boxes = boxes
+  }
+
+  // The below two getter methods both maps each element in the array to the Box class, and calls
+  // the appropriate method for calculating the wiring / shielding.
+  // Both getter methods return a single value, that is calculated by the reduce function.
+  get shielding() {
+    return this.boxes .map(box => new Box(box).shieldingPerBox)
+                      .reduce((accumulatedValue, currentBox) => accumulatedValue + currentBox)
+  }
+
+  get wiring() {
+    return this.boxes .map(box => new Box(box).wiringPerBox)
+                      .reduce((accumulatedValue, currentBox) => accumulatedValue + currentBox)
+  }
 }
 
+const example1 = `2x3x4
+1x10x1
+5x7x4`
 
-// ========= task two ========= \\ 
-// === Calculate the Wiring === \\
-// ============================ \\ 
+// Task 1
+console.log(new CalculateTotal(boxesListToArray(example1)).shielding)
 
-
-// Create a function to calculate the total volume and shortest circumferance of each box
-const wiringPerBox = box => {
-
-  // parse each string into an array and create three separate values to represent the length, width and height
-  // this value is sorted in ascending order, to help calculate which side will have the shortest circumferance
-  const boxArr = box.split('x').sort((a, b) => a - b);
-
-  // calculate the volume
-  const volume = boxArr[0] * boxArr[1] * boxArr[2]
-
-  // calculate the shortest circumferance, this is used to calculate the amount of extra slack to provision for each box
-  const shortestCircumferance = boxArr[0]*2 + boxArr[1]*2
-
-  // return the total sheeting required for the wiring
-  return volume + shortestCircumferance
-}
-
-// Now iterate through each box in the array
-const totalWiring = (list) => { return  list
-      // first, convert each string value for the box into the required allowance of sheeting, equal to volume + slack
-                                            .map( box => wiringPerBox(box))
-      // now calculate the total value of all boxes in the array. 
-                                            .reduce((accumulatedValue, currentBox) => accumulatedValue + currentBox)
-}
-
-
-
-// below is some simple DOM manipulation code, that can gives access to the index.html file, so we can visualise the results.
-const calcValues = () => {
-
-  // resets the input & output boxes
-  document.querySelector("#output").innerHTML = "";
-
-  // create three elements to append to document
-  let newBox = document.createElement('li')
-  let li = document.createElement("li")
-  let li2 = document.createElement("li")
-
-  // access the value inputted from the textarea
-  let inputVal = document.querySelector("#box-group").value 
-
-  // run the shielding and wiring functions and attach to identifiers
-  let shield = document.createTextNode(totalShielding(boxesListToArray(inputVal)))
-  let wire = document.createTextNode(totalWiring(boxesListToArray(inputVal)))
-
-  // append each element with text values
-  newBox.append("Next box group:")
-  li.append("Total shielding: ", shield)
-  li2.append("Total wiring: ", wire)
-
-  // append each element to the document
-  document.querySelector("#output").appendChild(newBox)
-  document.querySelector("#output").appendChild(li)
-  document.querySelector("#output").appendChild(li2)
-
-  // reset the input box
-  document.querySelector("#box-group").value = ""
-}
+// Task 2
+console.log(new CalculateTotal(boxesListToArray(example1)).wiring)
